0d49fc8361ff66a5c43f7d5d0bfe538d1b621879
Gustavo Santos <gustavo.santos@jbaysolutions.com>
2019-08-07 10:38:14 +0100

    webpack bundle analyser tests

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,15 @@
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = {
     configureWebpack: {
         output: {
             library: "VueGridLayout",
             libraryExport: 'default'
         },
+        // https://medium.com/js-dojo/how-to-reduce-your-vue-js-bundle-size-with-webpack-3145bf5019b7
+        // plugins: [new BundleAnalyzerPlugin()],
     },
     css: {
         extract: false
-    }
+    },
 }
