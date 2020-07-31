3e4599f087df0c916c797129ff8821c84d6b24e9
Gustavo Santos <gustavo.santos@jbaysolutions.com>
2020-07-31 10:24:40 +0100

    extra space fix

--- src/components/GridLayout.vue
+++ src/components/GridLayout.vue
@@ -273,8 +273,8 @@
                     compact(this.layout, this.verticalCompact);
                     this.eventBus.$emit("updateWidth", this.width);
                     this.updateHeight();
-                    
-                    this.$emit('layout-updated', this.layout)
+
+                    this.$emit('layout-updated',this.layout)
                 }
             },
             updateHeight: function () {
