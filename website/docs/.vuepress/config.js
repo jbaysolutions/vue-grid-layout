ad445fa6da8ae5e135ea5a7751a688a462ad227b
Mirakhmad Mirzajonov <m.mirakhmad@gmail.com>
2021-01-16 02:39:19 +0500

    Update config.js

--- website/docs/.vuepress/config.js
+++ website/docs/.vuepress/config.js
@@ -64,7 +64,7 @@ module.exports = {
 								'08-responsive-predefined-layouts',
 								'09-dynamic-add-remove',
 								'10-drag-from-outside',
-                '11-bounded',
+								'11-bounded',
 							]
 						}
 					]
@@ -109,7 +109,7 @@ module.exports = {
 								'08-responsive-predefined-layouts',
 								'09-dynamic-add-remove',
 								'10-drag-from-outside',
-                '11-bounded',
+								'11-bounded',
 							]
 						}
 					]
