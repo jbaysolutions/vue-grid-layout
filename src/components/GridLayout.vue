a025b411f9c37b2058ac765681a33c3323f832dd
ygj6 <yuangongji@foxmail.com>
2021-02-04 19:59:39 +0800

    Modify responsiceLayout when adding a layout item.

--- src/components/GridLayout.vue
+++ src/components/GridLayout.vue
@@ -129,6 +129,7 @@
                 layouts: {}, // array to store all layouts from different breakpoints
                 lastBreakpoint: null, // store last active breakpoint
                 originalLayout: null, // store original Layout
+                layoutsCache: this.responsiveLayouts
             };
         },
         created () {
@@ -279,6 +280,9 @@
                         }
 
                         this.lastLayoutLength = this.layout.length;
+                        this.layoutsCache = {
+                            [this.lastBreakpoint]: this.layout
+                        }
                         this.initResponsiveFeatures();
                     }
 
@@ -439,7 +443,7 @@
             // clear all responsive layouts
             initResponsiveFeatures(){
                 // clear layouts
-                this.layouts = Object.assign({}, this.responsiveLayouts);
+                this.layouts = Object.assign({}, this.layoutsCache);
             },
 
             // find difference in layouts
